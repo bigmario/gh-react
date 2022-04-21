6366fe2b85a9b8c62794a9668038fad6d82774a0
Mario Castro <mariocastro.pva@gmail.com>
2022-04-21 15:20:26 -0400

    fix on vite conf #2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://bigmariove.github.io/',
+  base: 'https://bigmario.github.io/',
   plugins: [react()]
 })
